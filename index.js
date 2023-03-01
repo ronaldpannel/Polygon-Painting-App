586be43a6e380003985001d9e7dbcc116b184c63
ronaldpannel <69686310+ronaldpannel@users.noreply.github.com>
2023-03-01 16:32:01 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ window.addEventListener("load", function () {
   canvas.height = window.innerHeight;
   const numberOfSidesSlider = document.getElementById("numberOfSidesSlider");
   const numberOfSidesLabel = document.getElementById("numberOfSidesLabel");
+  const rotationalOffsetSlider = document.getElementById("angleOffSetSlider");
+  const rotationalOffsetLabel = document.getElementById("angleOffSetLabel");
   const radiusSlider = document.getElementById("radiusSlider");
   const radiusSidesLabel = document.getElementById("radiusLabel");
+  const insetSlider = document.getElementById("insetSlider");
+  const insetSidesLabel = document.getElementById("insetLabel");
 
   ctx.strokeStyle = "pink";
   ctx.lineWidth = 2;
@@ -41,9 +45,9 @@ window.addEventListener("load", function () {
   let inset = 0.5;
   let n = 5;
   let rotationOffset = 0;
-   updateSliderLabels();
+  updateSliderLabels();
 
-  drawShape(50, 150, radius, inset, n);
+  drawShape(50, 600, radius, inset, n);
   let angle = 0;
   canvas.addEventListener("pointermove", function (e) {
     if (drawing) {
@@ -66,22 +70,32 @@ window.addEventListener("load", function () {
 
   numberOfSidesSlider.addEventListener("change", function (e) {
     n = e.target.value;
-    updateSliderLabels()
-    drawShape(50, 250, radius, inset, n);
+    updateSliderLabels();
+    drawShape(50, 550, radius, inset, n);
   });
 
-  radiusSlider.addEventListener('change', function(e){
-    radius = e.target.value
+  rotationalOffsetSlider.addEventListener("change", function (e) {
+    rotationOffset = e.target.value;
     updateSliderLabels();
-    drawShape(50, 250, radius, inset, n);
-  })
+    drawShape(50, 550, radius, inset, n);
+  });
+  radiusSlider.addEventListener("change", function (e) {
+    radius = e.target.value;
+    updateSliderLabels();
+    drawShape(50, 550, radius, inset, n);
+  });
+  insetSlider.addEventListener("change", function (e) {
+    inset = e.target.value;
+    updateSliderLabels();
+    drawShape(50, 550, radius, inset, n);
+  });
 
-  function updateSliderLabels(){
-      numberOfSidesLabel.innerText = `number of Sides: ${n}`;
-      radiusSidesLabel.innerText =`Radius; ${radius}`
+  function updateSliderLabels() {
+    numberOfSidesLabel.innerText = `number of Sides: ${n}`;
+    radiusSidesLabel.innerText = `Radius: ${radius}`;
+    rotationalOffsetLabel.innerText = `Rotational Offset: ${rotationOffset}`;
+    insetSidesLabel.innerText = `Inset Ratio: ${inset}`;
   }
-
- 
 
   //load function end
 });
